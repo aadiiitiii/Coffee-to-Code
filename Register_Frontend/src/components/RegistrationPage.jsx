5f3f41f255c6238f81cc2d8560e3f872870314d3
Palleti Reddy <palreddy@publicisgroupe.net>
2019-08-26 09:02:17 +0530

    Google oauth

--- Register_Frontend/src/components/RegistrationPage.jsx
+++ Register_Frontend/src/components/RegistrationPage.jsx
@@ -8,6 +8,7 @@
 import React, { Component } from 'react';
 import Recaptcha from 'react-recaptcha';
 import Axios from './Axios';
+import GoogleLogin from 'react-google-login';
 
 
 class RegistrationPage extends Component {
@@ -36,8 +37,10 @@ class RegistrationPage extends Component {
 
       fullList: [],
       securityQuestion1: [],
-      securityQuestion2: []
-
+      securityQuestion2: [],
+      firstnamereadonly: false,
+      lasstnamereadonly: false,
+      emailreadonly: false,
 
     };
 
@@ -64,9 +67,9 @@ class RegistrationPage extends Component {
   // confirmnpassword enters in the input box this will be fired 
   ConfirmPasswordEventHandler = (event) => {
     if (this.state.users.passwordHistory.pwd1 === event.target.value)
-      document.getElementById("conpasscheck").innerHTML="";
+      document.getElementById("conpasscheck").innerHTML = "";
     else
-    document.getElementById("conpasscheck").innerHTML="Password and Confirm Password should be same"
+      document.getElementById("conpasscheck").innerHTML = "Password and Confirm Password should be same"
   }
 
 
@@ -76,59 +79,59 @@ class RegistrationPage extends Component {
     // // code to validate all input cases
 
     // first name checking 
-    
+
     var ulen = this.state.users.firstName.length;
-    if(ulen < 2 || ulen > 10){
+    if (ulen < 2 || ulen > 10) {
       this.state.allcorrect = false;
       console.log("Length should be between 2 to 10")
     }
 
-    var fnameValid=this.state.users.firstName.match(/^[a-zA-Z]+$/)
-      fnameValid ? alert("FirstName is valid") : alert("FirstName is not valid");
+    var fnameValid = this.state.users.firstName.match(/^[a-zA-Z]+$/)
+    fnameValid ? alert("FirstName is valid") : alert("FirstName is not valid");
 
-    
 
-   //  last name checking
-     
-     
-      var    ulen=this.state.users.lastName.length;
-        if(ulen < 2 || ulen > 10){
-          this.state.allcorrect = false;
-        }
-        var lnameValid=this.state.users.lastName.match(/^[a-zA-Z]+$/)
-     lnameValid ? alert("LastName is  valid") : alert("LastName is not valid");
 
-   //   password checking
+    //  last name checking
 
-       var plen=this.state.users.passwordHistory.pwd1.length;
-    if(plen < 8 || plen > 15){
+
+    var ulen = this.state.users.lastName.length;
+    if (ulen < 2 || ulen > 10) {
+      this.state.allcorrect = false;
+    }
+    var lnameValid = this.state.users.lastName.match(/^[a-zA-Z]+$/)
+    lnameValid ? alert("LastName is  valid") : alert("LastName is not valid");
+
+    //   password checking
+
+    var plen = this.state.users.passwordHistory.pwd1.length;
+    if (plen < 8 || plen > 15) {
       this.state.allcorrect = false;
 
       var pwdValid = newpass.pwd1.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d.*)(?=.*\W.*)[a-zA-Z0-9\S]{8,15}$/);
-      if(pwdValid){
-        document.getElementById("passId").innerHTML="";
+      if (pwdValid) {
+        document.getElementById("passId").innerHTML = "";
       }
-      else{
-        document.getElementById("passId").innerHTML="Password should contain atleast one upper case,one lower case,one special character and one number";
-    }
+      else {
+        document.getElementById("passId").innerHTML = "Password should contain atleast one upper case,one lower case,one special character and one number";
+      }
 
-    //  email checking
+      //  email checking
 
       var emailValid = this.state.users.emailID.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-      if(emailValid){
+      if (emailValid) {
         alert("email is  valid")
       }
-      else{
+      else {
         alert("email is not valid")
       }
 
-   //   phone number
+      //   phone number
 
-      var pholen=this.state.users.phoneNo.length;
-    if(pholen !== 10){
-      this.state.allcorrect = false;
-    }
-      
+      var pholen = this.state.users.phoneNo.length;
+      if (pholen !== 10) {
+        this.state.allcorrect = false;
+      }
+
 
     }
 
@@ -158,7 +161,7 @@ class RegistrationPage extends Component {
 
   }
 
-  
+
 
   recaptchaLoaded = () => {
     console.log('captcha has loaded');
@@ -201,6 +204,21 @@ class RegistrationPage extends Component {
       this.setState({ users: { ...this.state.users, securityAns: { ...this.state.users.securityAns, securityAnsID2: answer } } });
   }
 
+  responseGoogleSuccess = (response) => {
+    console.log(response.profileObj);
+    this.setState({ users: { ...this.state.users, emailID: response.profileObj.email, firstName: response.profileObj.givenName, lastName: response.profileObj.familyName } }
+      , () => {
+        this.setState({ ...this.state, firstnamereadonly: true, lastnamereadonly: true, emailreadonly: true });
+      })
+      ;
+
+  }
+
+  responseGoogleFailure = (response) => {
+    console.log(response);
+  }
+
+
 
 
 
@@ -208,95 +226,105 @@ class RegistrationPage extends Component {
 
     return (
 
+      <div>
+        <form onSubmit={(e) => { this.validate(); e.preventDefault(); }}>
 
-      <form onSubmit={(e) => { this.validate(); e.preventDefault(); }}>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label" >FirstName</label>
-          <div className="col-sm-4">
-            <input type="text" className="form-control" placeholder="FirstName" required onChange={this.userDataEventHandler} name="firstName" value={this.state.users.firstName} />
-          </div>
-        </div>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">LastName</label>
-          <div className="col-sm-4">
-            <input type="text" className="form-control" placeholder="LastName" name="lastName" required onChange={this.userDataEventHandler} value={this.state.users.lastName} />
-          </div>
-        </div>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">EmailID</label>
-          <div className="col-sm-4">
-            <input type="email" className="form-control" placeholder="EmailID" name="emailID" required onChange={this.userDataEventHandler} value={this.state.users.emailID} />
-          </div>
-        </div>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">PhoneNo</label>
-          <div className="col-sm-4">
-            <input type="number" className="form-control" placeholder="PhoneNo" name="phoneNo" required onChange={this.userDataEventHandler} value={this.state.users.phoneNo} />
-          </div>
-        </div>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">Password</label>
-          <div className="col-sm-4">
-            <input type="password" className="form-control" placeholder="Password" name="pwd1" required onChange={this.PasswordEventHandler} value={this.state.users.passwordHistory.pwd1} />
-          </div>
-        </div>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">Confirm Password</label>
-          <div className="col-sm-4">
-            <input type="password" className="form-control" placeholder="Confirm Password" name="confirmPassword" required onChange={this.ConfirmPasswordEventHandler} />
-          </div>
-          <span id="conpasscheck"></span>
-        </div>
-
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">Security Question 1</label>
-          <div className="col-sm-3">
-            <select className="security" name="securityQueID1" required onChange={(e) => this.filterQuestion(e.target.value, 1)}>
-              {this.state.securityQuestion1.map(questions =>
-                <option key={questions.questionID} value={questions.questionID}> {questions.question} </option>
-              )}
-            </select>
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label" >FirstName</label>
+            <div className="col-sm-4">
+              <input type="text" className="form-control" placeholder="FirstName" required onChange={this.userDataEventHandler} name="firstName" value={this.state.users.firstName} />
+            </div>
           </div>
 
-          <div className="col-sm-4">
-            <input type="text" name="securityAnsID1" required value={this.state.users.securityAns.securityAnsID1} onChange={(e) => this.securityAnswers(e.target.value, 1)}></input>
-          </div>
-        </div>
-
-        <div className="form-group row">
-          <label className="col-sm-1 col-form-label">Security Question 2</label>
-          <div className="col-sm-3">
-            <select className="security" name="securityQueID2" required onChange={(e) => this.filterQuestion(e.target.value, 2)}>
-              {this.state.securityQuestion2.map(questions =>
-                <option key={questions.questionID} value={questions.questionID}>{questions.question}</option>
-              )}
-            </select>
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">LastName</label>
+            <div className="col-sm-4">
+              <input type="text" className="form-control" placeholder="LastName" name="lastName" required onChange={this.userDataEventHandler} value={this.state.users.lastName} />
+            </div>
           </div>
 
-          <div className="col-sm-4">
-            <input type="text" name="securityAnsID2" required value={this.state.users.securityAns.securityAnsID2} onChange={(e) => this.securityAnswers(e.target.value, 2)}></input>
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">EmailID</label>
+            <div className="col-sm-4">
+              <input type="email" className="form-control" placeholder="EmailID" name="emailID" required onChange={this.userDataEventHandler} value={this.state.users.emailID} />
+            </div>
           </div>
-        </div>
 
-        <Recaptcha
-          sitekey="6LcE97EUAAAAAJ-MJMDdEt2fX5KDN_pjsdbCQ-pJ"
-          render="explicit"
-          onloadCallback={this.recaptchaLoaded}
-          verifyCallback={this.verifyCallback}
-        />
-        <br />
-        <button className="btn btn-primary" type="submit">Submit</button>
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">PhoneNo</label>
+            <div className="col-sm-4">
+              <input type="number" className="form-control" placeholder="PhoneNo" name="phoneNo" required onChange={this.userDataEventHandler} value={this.state.users.phoneNo} />
+            </div>
+          </div>
 
-      </form>
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">Password</label>
+            <div className="col-sm-4">
+              <input type="password" className="form-control" placeholder="Password" name="pwd1" required onChange={this.PasswordEventHandler} value={this.state.users.passwordHistory.pwd1} />
+            </div>
+          </div>
+
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">Confirm Password</label>
+            <div className="col-sm-4">
+              <input type="password" className="form-control" placeholder="Confirm Password" name="confirmPassword" required onChange={this.ConfirmPasswordEventHandler} />
+            </div>
+            <span id="conpasscheck"></span>
+          </div>
 
 
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">Security Question 1</label>
+            <div className="col-sm-3">
+              <select className="security" name="securityQueID1" required onChange={(e) => this.filterQuestion(e.target.value, 1)}>
+                {this.state.securityQuestion1.map(questions =>
+                  <option key={questions.questionID} value={questions.questionID}> {questions.question} </option>
+                )}
+              </select>
+            </div>
+
+            <div className="col-sm-4">
+              <input type="text" name="securityAnsID1" required value={this.state.users.securityAns.securityAnsID1} onChange={(e) => this.securityAnswers(e.target.value, 1)}></input>
+            </div>
+          </div>
+
+          <div className="form-group row">
+            <label className="col-sm-1 col-form-label">Security Question 2</label>
+            <div className="col-sm-3">
+              <select className="security" name="securityQueID2" required onChange={(e) => this.filterQuestion(e.target.value, 2)}>
+                {this.state.securityQuestion2.map(questions =>
+                  <option key={questions.questionID} value={questions.questionID}>{questions.question}</option>
+                )}
+              </select>
+            </div>
+
+            <div className="col-sm-4">
+              <input type="text" name="securityAnsID2" required value={this.state.users.securityAns.securityAnsID2} onChange={(e) => this.securityAnswers(e.target.value, 2)}></input>
+            </div>
+          </div>
+
+          <Recaptcha
+            sitekey="6LcE97EUAAAAAJ-MJMDdEt2fX5KDN_pjsdbCQ-pJ"
+            render="explicit"
+            onloadCallback={this.recaptchaLoaded}
+            verifyCallback={this.verifyCallback}
+          />
+          <br />
+          <button className="btn btn-primary" type="submit">Submit</button>
+
+        </form>
+
+        <br></br>
+        <GoogleLogin
+          clientId={'850784983032-gk55huo5976bjbrtgvi403rufcvg579s.apps.googleusercontent.com'}
+          onSuccess={this.responseGoogleSuccess}
+          onFailure={this.responseGoogleFailure}
+        >
+
+          <span > Signup with Google</span>
+        </GoogleLogin>
+
+      </div>
     )
   }
 }
